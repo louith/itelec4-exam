c4aeee8565cfef98961d9be3539b72a6bbb17059
louith <sepradoloys@gmail.com>
2023-04-01 22:35:35 +0800

    hdhsdshd

---
+++ src/data.js
@@ -0,0 +1,40 @@
+let products = [
+    {
+        id: 1,
+        name: 'Baroque Pearl Huggie',
+        price: 500.00,
+        category: 'Apparel',
+        imgPath: require('./assets/img/Baroque Pearl Huggie.png')
+    },
+    {
+        id: 2,
+        name: 'Bezel Heart Charm Huggie',
+        price: 350.00,
+        category: 'Stickers',
+        imgPath: require('./assets/img/Bezel Heart Charm Huggie.png')
+        
+    },
+    {
+        id: 3,
+        name: 'Chained Pave Huggie',
+        price: 250.00,
+        category: 'Hat',
+        imgPath: require('./assets/img/Chained Pave Huggie.png')
+    },
+    {
+        id: 4,
+        name: 'Green Mini Flatback Stud',
+        price: 200.00,
+        category: 'Accessories',
+        imgPath: require('./assets/img/Green Mini Flatback Stud.png')
+    },
+    {
+        id: 5,
+        name: 'Pave Evil Eye Stud',
+        price: 350.00,
+        category: 'Footwear',
+        imgPath: require('./assets/img/Pave Evil Eye Stud.png')
+    }
+]
+
+export default products
